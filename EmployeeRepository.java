111275b9d3088db2c37537b6f1d4ef649f8cabfc
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-08-09 15:03:27 +0530

    Add EmployeeRepository interface for employee data access

---
+++ EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.reethika.employeeapi.repository;
+
+import com.reethika.employeeapi.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    List<Employee> findByDepartmentIgnoreCase(String department);
+}
